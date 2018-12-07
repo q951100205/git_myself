8d3ac0858cb0dab65f0f8a513bd586c3f55c37ae
guoxinlong <951100205@qq.com>
2018-12-07 17:24:35 +0800

    新建的User，提交到本地仓库

---
+++ git/src/main/java/com/itheima/domain/User.java
@@ -0,0 +1,12 @@
+package com.itheima.domain;
+
+/**
+ * describe:
+ * package:com.itheima.domain
+ * author:     GXL
+ * date:     2018/12/7
+ */
+public class User {
+    private Integer id;
+    private String name;
+}
